a4642ec167506bf6cd96f35b4b4b2a1ef50cd91c
aFrInaTi0n <creum@devserver.(none)>
2013-02-08 09:54:43 +0100

    Version 735

--- EbatNs_ServiceProxyShopping.php
+++ EbatNs_ServiceProxyShopping.php
@@ -21,7 +21,7 @@ require_once 'EbatNs_Session.php';
 /**
  * The WSDL version the SDK is built against.
  */
-define('EBAY_WSDL_VERSION', '733');
+define('EBAY_WSDL_VERSION', '735');
 
 /**
  * This class is the basic interface to the eBay-Webserice for the user.
--- samples/sampleshopping_GetShippingCosts.php
+++ samples/sampleshopping_GetShippingCosts.php
@@ -15,7 +15,7 @@ require_once 'EbatNs_EnvironmentShopping.php';
  * @subpackage samples_shopping
  * @author johann 
  * @copyright Copyright (c) 2008
- * @version $Id: sampleshopping_GetShippingCosts.php,v 1.73 2011-12-29 12:27:18 michaelcoslar Exp $
+ * @version $Id: sampleshopping_GetShippingCosts.php,v 1.74 2011-12-29 12:29:15 michaelcoslar Exp $
  * @access public 
  */
 class sample_GetShippingCosts extends EbatNs_EnvironmentShopping
