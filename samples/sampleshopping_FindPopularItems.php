d350337633c7230a94617f3789b74a49c8daf1b6
aFrInaTi0n <creum@devserver.(none)>
2013-02-08 09:54:45 +0100

    Version 781

--- samples/sampleshopping_FindPopularItems.php
+++ samples/sampleshopping_FindPopularItems.php
@@ -15,7 +15,7 @@ require_once 'EbatNs_EnvironmentShopping.php';
  * @subpackage samples_shopping
  * @author johann 
  * @copyright Copyright (c) 2008
- * @version $Id: sampleshopping_FindPopularItems.php,v 1.90 2012-09-10 11:08:04 michaelcoslar Exp $
+ * @version $Id: sampleshopping_FindPopularItems.php,v 1.91 2012-09-10 11:09:10 michaelcoslar Exp $
  * @access public 
  */
 class sample_FindPopularItems extends EbatNs_EnvironmentShopping
